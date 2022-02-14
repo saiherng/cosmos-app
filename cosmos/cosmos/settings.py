f67f8da5e894bb756230fcf6649306dffdba65e2
saiherng <saiherng14@gmail.com>
2022-02-14 19:14:52 +0630

    prep

--- cosmos/cosmos/settings.py
+++ cosmos/cosmos/settings.py
@@ -12,18 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
-import os
-import environ
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-environ.Env.read_env(env_file=os.path.join(BASE_DIR, ".env"))
 
 
 # Quick-start development settings - unsuitable for production
