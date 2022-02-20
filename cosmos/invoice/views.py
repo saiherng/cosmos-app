4641e163f1788c82dc2f20c1ecfa8d22f018dffa
saiherng <saiherng14@gmail.com>
2022-02-20 22:25:22 +0630

    added bootstrap

--- cosmos/invoice/views.py
+++ cosmos/invoice/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 
 
 def index(request):
-
     return render(request, 'invoice/index.html')
+
+def create_invoice(request):
+    return render(request, 'invoice/invoice.html')
+
 # Create your views here.
