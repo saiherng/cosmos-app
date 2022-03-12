c5b395268a88c23c20976021d506bb427aef55a6
saiherng <saiherng14@gmail.com>
2022-03-12 19:31:22 +0630

    fixed view n model

--- cosmos/invoice/views.py
+++ cosmos/invoice/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import *
+
 
 def index(request):
-    return render(request, 'invoice/index.html')
+
+    invoice = Invoice.objects.get(pk=1)
+    customer = invoice.customer
+    
+
+    context = {
+        'invoice' : invoice,
+        'customer': customer
+        
+       
+    }
+
+    return render(request, 'invoice/index.html', context)
 
 def create_invoice(request):
+
+   
     return render(request, 'invoice/invoice.html')
+
+
 
 # Create your views here.
